4604ee6367ecc49eac0145cffab79ea92e3737c5
tjdals504 <sungminro92@gmail.com>
2017-03-27 01:15:01 -0400

    Changes

--- routes/users.js
+++ routes/users.js
@@ -22,23 +22,52 @@ router.post('/', authHelpers.createSecure, function(req, res){
 		email: req.body.email,
 	  username: req.body.username,
 	  password: res.hashedPassword,
-	  travelCountry: res.body.travelCountry
+	  travelCountry: req.body.travelCountry
 	});
 
 	user.save(function(err, user){
 		if (err) console.log(err);
 		//console.log(user);
-		res.redirect('/users/' + user.id);
+		res.redirect('/user/' + user.id);
 	});
 
 });
 
 // CREATE A NEW TRACK on user/index.hbs.
+// ROUTE TO VIEW new.hbs
+router.get('/:id/new', function(req, res){
+	User.findById(req.params.id)
+	.exec(function(err, user){
+		console.log(user.id)
+		console.log(user.tracks)
+		res.render('tracks/new.hbs', {
+			user: user,
+			tracks: user.tracks
+		});
+	});
+});
+
+// CREATE A NEW TRACK ROUTE
 router.post('/:id', function(req, res){
 	User.findById(req.params.id)
 	.exec(function(err, user){
-		user.tracks.push(new Track
-	})
-})
+		user.tracks.push(new Track({
+			title: req.body.title,
+			location: req.body.location,
+			imgUrl: req.body.imgUrl
+		}));
+		user.save(function(err){
+			res.send(user);
+		});
+	});
+});
+
+// // DELETE A TRACK ROUTE
+// router.delete('/:id', function(req, res){
+// 	User.findById(req.params.id)
+// 	.exec(function(err, user){
+// 		user.tracks.
+// 	})
+// }
 
 module.exports = router;
